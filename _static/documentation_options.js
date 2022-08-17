ad7d73e097d16f0b770ef6db06d93c58e9a7447f
Jmiguel17 <jose.gonzalez18@outlook.com>
2022-08-17 21:12:02 +0000

    Deploying to gh-pages from  @ 72d7293e7b76fffcbcd9c07bb86e02267a3dd483 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.1.dev151+gc101649',
-    LANGUAGE: 'None',
+    VERSION: '1.5.1.dev160+g72d7293',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
