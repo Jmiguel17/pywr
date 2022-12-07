39990472b8f1de2ef2bf5c5efa557df510a376ca
Jmiguel17 <jose.gonzalez18@outlook.com>
2022-12-07 21:38:25 +0000

    Deploying to gh-pages from  @ 7ef5f24d34649a842433ff0e8d23d61cef641392 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.1.dev161+g7f815b3',
+    VERSION: '1.5.1.dev167+g7ef5f24',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
